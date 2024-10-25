eb1cdc67978d7773f84e46ee6970bffd5791527d
邢敏亮 <minliang.xing@medbanks.cn>
2024-10-25 17:00:40 +0800

    ## 0.3.0.6 ### Supported - Support comment on export `puml` .

--- src/main/java/vanstudio/sequence/formatter/PlantUMLFormatter.java
+++ src/main/java/vanstudio/sequence/formatter/PlantUMLFormatter.java
@@ -2,6 +2,7 @@ package vanstudio.sequence.formatter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import vanstudio.sequence.config.SequenceSettingsState;
 import vanstudio.sequence.openapi.Constants;
@@ -19,33 +20,69 @@ public class PlantUMLFormatter implements IFormatter {
         buffer.append("participant Actor").append('\n');
         String classA = callStack.getMethod().getClassDescription().getClassShortName();
         String method = getMethodName(callStack.getMethod());
+
+        boolean showCallNumbers = SequenceSettingsState.getInstance().SHOW_CALL_NUMBERS;
+
+        ArrayList<Integer> seqs = new ArrayList<>();
+        String seq = "";
+        if (showCallNumbers) {
+            seqs.add(1);
+            seq = seqs.stream().map(String::valueOf).collect(Collectors.joining("."));
+
+        }
+
         if (Constants.CONSTRUCTOR_METHOD_NAME.equals(callStack.getMethod().getMethodName())) {
+            if (showCallNumbers) {
+                buffer.append(seq).append(":");
+            }
             buffer.append("create ").append(classA).append('\n');
         }
-        buffer.append("Actor").append(" -> ").append(classA).append(" : ").append(method).append('\n');
+
+        buffer.append("Actor").append(" -> ").append(classA).append(" : ");
+        if (showCallNumbers) {
+            buffer.append(seq).append(":");
+        }
+        buffer.append(method).append('\n');
         buffer.append("activate ").append(classA).append('\n');
-        generate(buffer, callStack);
+        generate(buffer, callStack, seqs);
         buffer.append("return").append('\n');
         buffer.append("@enduml");
         return buffer.toString();
     }
 
-    private void generate(StringBuffer buffer, CallStack parent) {
+    private void generate(StringBuffer buffer, CallStack parent, List<Integer> parentSeqs) {
         String classA = parent.getMethod().getClassDescription().getClassShortName();
+        boolean showCallNumbers = SequenceSettingsState.getInstance().SHOW_CALL_NUMBERS;
 
-        for (CallStack callStack : parent.getCalls()) {
+        for (int i = 0; i < parent.getCalls().size(); i++) {
+            ArrayList<Integer> innerSeqs = new ArrayList<>();
+            String seq = "";
+            if (showCallNumbers) {
+                innerSeqs.addAll(parentSeqs);
+                innerSeqs.add(i + 1);
+                seq = innerSeqs.stream().map(String::valueOf).collect(Collectors.joining("."));
+            }
+
+            CallStack callStack = parent.getCalls().get(i);
             String classB = callStack.getMethod().getClassDescription().getClassShortName();
             String method = getMethodName(callStack.getMethod());
+
             if (Constants.CONSTRUCTOR_METHOD_NAME.equals(callStack.getMethod().getMethodName())) {
+                if (showCallNumbers) {
+                    buffer.append(seq).append(":");
+                }
                 buffer.append("create ").append(classB).append('\n');
             }
-            buffer.append(classA).append(" -> ").append(classB).append(" : ").append(method).append('\n');
+            buffer.append(classA).append(" -> ").append(classB).append(" : ");
+            if (showCallNumbers) {
+                buffer.append(seq).append(":");
+            }
+            buffer.append(method).append('\n');
             buffer.append("activate ").append(classB).append('\n');
-            generate(buffer, callStack);
+            generate(buffer, callStack, innerSeqs);
             buffer.append(classB).append(" --> ").append(classA).append('\n');
             buffer.append("deactivate ").append(classB).append('\n');
         }
-
 
     }
 
